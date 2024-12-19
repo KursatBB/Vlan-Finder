6a67c280fea39910130c69cbbbf2009fe0002db9
Kürşat <66494923+KursatBB@users.noreply.github.com>
2024-12-19 16:42:51 +0300

    Update opensmb.py

--- opensmb.py
+++ opensmb.py
@@ -1,14 +1,14 @@
 # Metin dosyasını okuyup açık portlara sahip IP adreslerini bulmak
 open_ports = []
 
-with open("nmap_output.txt", "r") as file:  # Dosyanızın adı nmap_output.txt olabilir
+with open("nmap_output.txt", "r") as file:  # Dosyanızın adı
     lines = file.readlines()
 
 for i, line in enumerate(lines):
     if "445/tcp open" in line:  # "445/tcp open" geçen satırları bul
-        # Bir önceki satırda IP adresi bulunuyor
-        if i > 0 and "Nmap scan report for" in lines[i-1]:
-            ip_address = lines[i-1].strip().split()[-1]  # IP adresini al
+        # 4 satır öncesindeki satırda IP adresi bulunuyor
+        if i >= 4 and "Nmap scan report for" in lines[i-4]:
+            ip_address = lines[i-4].strip().split()[-1]  # IP adresini al
             open_ports.append(ip_address)
 
 # Bulunan IP adreslerini yazdır
